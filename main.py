12918c7e64b23b51c7e73f281c1ce6e84242b3bb
Chris Yates <chris.yates@sage.com>
2019-10-01 17:39:45 +0100

    bgmnxcfhb

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-    import os
-    import pymsteams
-    import json
-    import re
-    import sys
+import os
+import pymsteams
+import json
+import re
+import sys
     
-    GITHUB_REPOSITORY = os.environ['GITHUB_REPOSITORY']
-    GITHUB_ACTOR = os.environ['GITHUB_ACTOR']
-    GITHUB_SHA = os.environ['GITHUB_SHA']
-    GITHUB_WORKFLOW = os.environ['GITHUB_WORKFLOW']
-    GITHUB_EVENT_NAME = os.environ['GITHUB_EVENT_NAME']
-    GITHUB_EVENT_PATH = os.environ['GITHUB_EVENT_PATH']
-    GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-    GITHUB_REF = os.environ['GITHUB_REF']
-    BRANCH = re.sub('(refs\/heads\/)',r'',GITHUB_REF)
-    with open(GITHUB_EVENT_PATH, 'r') as json_file:
-     data = json.load(json_file)
-    pushMessage = data['head_commit']['message']
-    compareUrl = data['compare']
-    print "Message: "+pushMessage
-    print "CompareURL: "+compareUrl
-    print "GITHUB_SHA: "+GITHUB_SHA
-    print 'Number of arguments:', len(sys.argv), 'arguments.'
-    print 'Argument List:', str(sys.argv)
+GITHUB_REPOSITORY = os.environ['GITHUB_REPOSITORY']
+GITHUB_ACTOR = os.environ['GITHUB_ACTOR']
+GITHUB_SHA = os.environ['GITHUB_SHA']
+GITHUB_WORKFLOW = os.environ['GITHUB_WORKFLOW']
+GITHUB_EVENT_NAME = os.environ['GITHUB_EVENT_NAME']
+GITHUB_EVENT_PATH = os.environ['GITHUB_EVENT_PATH']
+GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+GITHUB_REF = os.environ['GITHUB_REF']
+BRANCH = re.sub('(refs\/heads\/)',r'',GITHUB_REF)
+with open(GITHUB_EVENT_PATH, 'r') as json_file:
+ data = json.load(json_file)
+pushMessage = data['head_commit']['message']
+compareUrl = data['compare']
+print "Message: "+pushMessage
+print "CompareURL: "+compareUrl
+print "GITHUB_SHA: "+GITHUB_SHA
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
