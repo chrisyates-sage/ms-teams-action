816857ad6556e2367eb5a4f0b8ef7d03e3c32809
Chris Yates <chris.yates@sage.com>
2019-10-03 14:58:00 +0100

    frgafrg

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-	  // `who-to-greet` input defined in action metadata file
-	  const nameToGreet = core.getInput('message');
-	  console.log(`Hello ${nameToGreet}!`);
-	  const time = (new Date()).toTimeString();
-	  core.setOutput("time", time);
+	  // `message` input defined in action metadata file
+	  //const nameToGreet = core.getInput('message');
+	  //console.log(`Hello ${nameToGreet}!`);
+	  //const time = (new Date()).toTimeString();
+	  //core.setOutput("time", time);
 	  // Get the JSON webhook payload for the event that triggered the workflow
 	  const payload = JSON.stringify(github.context.payload, undefined, 2)
 	  console.log(`The event payload: ${payload}`);
+	  console.log(`Test1: ${payload.head_commit.message}`)
+	  console.log(`Test2: ${process.env.GITHUB_REPOSITORY}`)
+	  
 } catch (error) {
 	  core.setFailed(error.message);
 }
