b03c3e85aeae3da5c49d92baa7e2a6db3290f670
Chris Yates <chris.yates@sage.com>
2019-10-04 09:47:43 +0100

    retrhsgt

--- index.js
+++ index.js
@@ -10,9 +10,56 @@ try {
 	  // Get the JSON webhook payload for the event that triggered the workflow
 	  const payload = JSON.stringify(github.context.payload, undefined, 2)
 	  console.log(`The event payload: ${payload}`);
-	  console.log(`Test1: ${github.context.payload.head_commit.message}`)
-	  console.log(`Test2: ${process.env.GITHUB_REPOSITORY}`)
+	  //console.log(`Test1: ${github.context.payload.head_commit.message}`)
+	  //console.log(`Test2: ${process.env.GITHUB_REPOSITORY}`)
+	  jsondata = new Object()
+	  //var potentialAction
+	  jsondata.text = '_Jekyll (release) CI_ :- New build run from push event by'
+	  jsondata.title = 'New build run triggered from chrisyates-sage/Arcana-Jekyll-Theme'
+
+	  var action1 = {
+		'@context': 'http://schema.org',
+		'target': 'https://github.com/chrisyates-sage/Arcana-Jekyll-Theme/compare/b4e2f0adb692...43cbd03d5abb',
+		'@type': 'ViewAction',
+		'name': 'Go to repo'	
+	  }
+	  var fact1 = {
+		'name': 'Repository',
+		'value': 'chrisyates-sage/Arcana-Jekyll-Theme'
+	  }
+	  var fact2 = {
+		'name': 'Commit SHA',
+		'value': 'c14fdae25f224c1d7a154b6f9759b5ae87de0b62'
+	  }
+	  jsondata['potentialAction'] = []
+	  jsondata['potentialAction'].push(action1)
+	  jsondata['sections'] = []
 	  
+	  var section1 = new Object()
+	  section1['facts'] = []
+	  section1['facts'].push(fact1)
+	  section1['facts'].push(fact2)
+	  section1['activityImage'] = 'https://github.com/chrisyates-sage.png?size=48'
+	  section1['activityText'] = 'Msg: rsaelkjhradf'
+	  section1['activityTitle'] = 'chrisyates-sage'
+	  jsondata['sections'].push(section1)
+	  
+	  const jsonout = JSON.stringify(jsondata, undefined, 2)
+	  //console.log(`data: ${jsonout}`)
+
+	  const myPost = async () => {
+		const rawResponse = await fetch('https://outlook.office.com/webhook/4debbfbb-c394-4de0-a0eb-3cd444c9554e@3e32dd7c-41f6-492d-a1a3-c58eb02cf4f8/IncomingWebhook/2aac373336894c12b3db5d43c350af17/2b82dd20-1338-4397-807d-e05538020cef', {
+		  method: 'POST',
+		  //headers: {
+			//'Accept': 'application/json',
+			//'Content-Type': 'application/json'
+		  //},
+		  body: jsonout
+		});
+		const response = await rawResponse.json();
+		console.log(response);
+	};
+
 } catch (error) {
 	  core.setFailed(error.message);
 }
