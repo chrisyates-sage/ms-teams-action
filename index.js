eafc7759e203fc0c0134f19170062b5ae8181a4d
Chris Yates <chris.yates@sage.com>
2019-10-07 09:04:39 +0100

    fgafrg

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ try {
 	  //core.setOutput("time", time);
 	  // Get the JSON webhook payload for the event that triggered the workflow
 	  const payload = JSON.stringify(github.context.payload, undefined, 2)
-	  console.log(`The event payload: ${payload}`);
-	  //console.log(`Test1: ${github.context.payload.head_commit.message}`)
-	  //console.log(`Test2: ${process.env.GITHUB_REPOSITORY}`)
+	  //console.log(`The event payload: ${payload}`);
+	  console.log(`Test1: ${github.context.payload.head_commit.message}`)
+	  console.log(`Test2: ${process.env.GITHUB_REPOSITORY}`)
 	  jsondata = new Object()
-	  //var potentialAction
 	  jsondata.text = '_Jekyll (release) CI_ :- New build run from push event by'
 	  jsondata.title = 'New build run triggered from chrisyates-sage/Arcana-Jekyll-Theme'
 
 	  var action1 = {
 		'@context': 'http://schema.org',
-		'target': 'https://github.com/chrisyates-sage/Arcana-Jekyll-Theme/compare/b4e2f0adb692...43cbd03d5abb',
+		'target': ["https://github.com/chrisyates-sage/Arcana-Jekyll-Theme/compare/b4e2f0adb692...43cbd03d5abb"],
 		'@type': 'ViewAction',
 		'name': 'Go to repo'	
 	  }
@@ -40,22 +39,21 @@ try {
 	  section1['facts'].push(fact1)
 	  section1['facts'].push(fact2)
 	  section1['activityImage'] = 'https://github.com/chrisyates-sage.png?size=48'
-	  section1['activityText'] = 'Msg: rsaelkjhradf'
+	  section1['activityText'] = github.context.payload.head_commit.message
 	  section1['activityTitle'] = 'chrisyates-sage'
 	  jsondata['sections'].push(section1)
 	  
 	  const jsonout = JSON.stringify(jsondata, undefined, 2)
-	  //console.log(jsonout)
+	  console.log(jsonout)
 
 	var url = "https://outlook.office.com/webhook/4debbfbb-c394-4de0-a0eb-3cd444c9554e@3e32dd7c-41f6-492d-a1a3-c58eb02cf4f8/IncomingWebhook/2aac373336894c12b3db5d43c350af17/2b82dd20-1338-4397-807d-e05538020cef";
 
 	const https = require('https');
-	var postData = JSON.stringify({msg: 'Hello World!'})
     var options = {
 	   hostname: 'outlook.office.com',
 	   path: '/webhook/4debbfbb-c394-4de0-a0eb-3cd444c9554e@3e32dd7c-41f6-492d-a1a3-c58eb02cf4f8/IncomingWebhook/2aac373336894c12b3db5d43c350af17/2b82dd20-1338-4397-807d-e05538020cef',
 	   method: 'POST',
-	   headers: { 'Content-Type': 'application/json' }
+	   headers: { 'Content-Type': 'application/json', "Content-Length": jsonout.length }
 	};
 	var req = https.request(options, (res) => {
 		console.log('statusCode:', res.statusCode);
@@ -70,7 +68,7 @@ try {
 		console.error(e);
 	});
 	  
-	req.write(postData);
+	req.write(jsonout);
 	req.end();
 
 } catch (error) {
