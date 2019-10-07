400cbd270041186b5b46ae29ead3066b4595f932
Chris Yates <chris.yates@sage.com>
2019-10-07 10:53:22 +0100

    stjh

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-	  // `message` input defined in action metadata file
-	  //const nameToGreet = core.getInput('message');
-	  //console.log(`Hello ${nameToGreet}!`);
 	  //const time = (new Date()).toTimeString();
 	  //core.setOutput("time", time);
 	  // Get the JSON webhook payload for the event that triggered the workflow
 	  const payload = JSON.stringify(github.context.payload, undefined, 2)
-	  //console.log(`The event payload: ${payload}`);
-	  console.log(`Test1: ${github.context.payload.head_commit.message}`)
-	  console.log(`Test2: ${process.env.GITHUB_REPOSITORY}`)
 	  jsondata = new Object()
 	  jsondata.text = `_${process.env.GITHUB_WORKFLOW}_ :- New build run from push event by`
 	  jsondata.title = `New build run triggered from ${process.env.GITHUB_REPOSITORY}`
@@ -46,9 +40,10 @@ try {
 	  const jsonout = JSON.stringify(jsondata, undefined, 2)
 	  console.log(jsonout)
 
-	var url = "https://outlook.office.com/webhook/4debbfbb-c394-4de0-a0eb-3cd444c9554e@3e32dd7c-41f6-492d-a1a3-c58eb02cf4f8/IncomingWebhook/2aac373336894c12b3db5d43c350af17/2b82dd20-1338-4397-807d-e05538020cef";
-	//const url = core.getInput('message');
-	
+	//var url = "https://outlook.office.com/webhook/4debbfbb-c394-4de0-a0eb-3cd444c9554e@3e32dd7c-41f6-492d-a1a3-c58eb02cf4f8/IncomingWebhook/2aac373336894c12b3db5d43c350af17/2b82dd20-1338-4397-807d-e05538020cef";
+	const url = core.getInput('teamsurl');
+	console.log(`Url passed: ${url}`)
+
 	const https = require('https');
     var options = {
 	   hostname: 'outlook.office.com',
@@ -60,9 +55,7 @@ try {
 		console.log('statusCode:', res.statusCode);
 		console.log('headers:', res.headers);
 	  
-		res.on('data', (d) => {
-		  process.stdout.write(d);
-		});
+		res.on('data', (d) => {  process.stdout.write(d);  });
 	});
 	  
 	req.on('error', (e) => {
